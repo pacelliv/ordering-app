8d3da3ffeb6a064fac13485d7b2740a1cc3c6a92
PacelliV <flores.eugenio03@gmail.com>
2022-11-22 15:13:51 -0500

    gh-pages added

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:pacelliv/tenzies.git master:gh-pages
+git push -f git@github.com:pacelliv/ordering-app.git master:gh-pages
 
 cd -
